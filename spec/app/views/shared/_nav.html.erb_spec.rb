0a536061c539fc9360e72a26a7ab270b048ed754
dripster82 <paul@ketelle.co.uk>
2023-03-17 11:36:49 +0000

    fixed spec issue with nav

--- spec/app/views/shared/_nav.html.erb_spec.rb
+++ spec/app/views/shared/_nav.html.erb_spec.rb
@@ -13,10 +13,21 @@ RSpec.describe 'shared/_nav.html.erb', type: :view do
 
   context 'when on the home page' do
     it 'adds the active class to the home link' do
+      allow(view).to receive(:current_page?).and_return(false)
       allow(view).to receive(:current_page?).with(root_path).and_return(true)
       render
       assert_select 'a', count: 3
       assert_select 'a.active', text: 'Home'.to_s, count: 1
+    end
+  end
+
+  context 'when on the projects page' do
+    it 'adds the active class to the project link' do
+      allow(view).to receive(:current_page?).and_return(false)
+      allow(view).to receive(:current_page?).with(projects_path).and_return(true)
+      render
+      assert_select 'a', count: 3
+      assert_select 'a.active', text: 'Projects'.to_s, count: 1
     end
   end
 end
